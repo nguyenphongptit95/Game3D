966c488a83fcd11338024eb82bc1ecfd4bf26781
Phong <oogiovamayoo2013@gmail.com>
2018-02-02 00:01:05 +0700

    1
    1

---
+++ Project3D/Assets/Script/Character.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var myBody: Rigidbody;
+
+function Awake(){
+	myBody = gameObject.GetComponent(Rigidbody);
+}
+
+function Start () {
+
+}
+
+function FixedUpdate () {
+	var v3Position = myBody.transform.localPosition;
+	v3Position.z += 2 * Time.deltaTime;
+	myBody.transform.localPosition = v3Position;
+}
